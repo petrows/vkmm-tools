22ba065250b9b84b3bacdc70f74c99cd0816c5ef
Peter Golovachev <p.golovachev@s-e.su>
2015-10-12 14:23:52 +0300

    teamcity

---
+++ teamcity.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Exit on command error
+set -e
+
+TOOL_CMAKE="cmake.exe"
+TOOL_MSBUILD="MSBuild.exe"
+
+rm -rf build
+mkdir -p build
+mkdir -p build/out
+cd build
+
+$TOOL_CMAKE .. -G "Visual Studio 11" -T "v110_xp" -DCMAKE_INSTALL_PREFIX="$(pwd)"
+
+$TOOL_MSBUILD /p:Configuration=Release /m:2 /p:BuildInParallel=true INSTALL.vcxproj
